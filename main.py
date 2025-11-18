5fcc413b344b95db326fd5b040360d238de9cff3
markgallant01 <markgallant01@gmail.com>
2025-11-18 00:32:29 -0500

    almost done

--- main.py
+++ main.py
@@ -4,17 +4,13 @@ import os
 import random
 import pygame as pg
 
-# window constants
-(SCREEN_WIDTH, SCREEN_HEIGHT) = 1280, 720
-SCREEN_CENTER: tuple[float,float] = SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2
-
-
 class Player(pg.sprite.Sprite):
-    def __init__(self, surface: pg.surface.Surface,
+    def __init__(self, player_surface: pg.surface.Surface,
+                 laser_surface: pg.surface.Surface,
                  group: pg.sprite.Group[pg.sprite.Sprite]):
         super().__init__(group)
 
-        self.image = surface
+        self.image = player_surface
         self.rect: pg.FRect = self.image.get_frect(center = SCREEN_CENTER)
         self.direction = pg.Vector2()
         self.speed = 300
@@ -23,6 +19,8 @@ class Player(pg.sprite.Sprite):
         self.can_shoot = True
         self.laser_shoot_time = 0
         self.cooldown_duration = 400
+
+        self.laser_surface = laser_surface
 
     def laser_timer(self):
         if not self.can_shoot:
@@ -40,7 +38,8 @@ class Player(pg.sprite.Sprite):
 
         recent_keys = pg.key.get_just_pressed()
         if recent_keys[pg.K_SPACE] and self.can_shoot:
-            Laser(self.rect.midtop, all_sprites)
+            Laser(self.rect.midtop, self.laser_surface,
+                  [all_sprites, laser_sprites])
             self.can_shoot = False
             self.laser_shoot_time = pg.time.get_ticks()
 
@@ -61,8 +60,8 @@ class Star(pg.sprite.Sprite):
 class Laser(pg.sprite.Sprite):
     def __init__(self, position: tuple[float,float],
                  surface: pg.surface.Surface,
-                 group: pg.sprite.Group[pg.sprite.Sprite]):
-        super().__init__(group)
+                 groups: list[pg.sprite.Group[pg.sprite.Sprite]]):
+        super().__init__(groups)
         self.image = surface
         self.rect: pg.FRect = self.image.get_frect(midbottom = position)
 
@@ -76,23 +75,54 @@ class Laser(pg.sprite.Sprite):
 
 class Meteor(pg.sprite.Sprite):
     def __init__(self, surface: pg.surface.Surface,
-                 group: pg.sprite.Group[pg.sprite.Sprite]):
-        super().__init__(group)
+                 groups: list[pg.sprite.Group[pg.sprite.Sprite]]):
+        super().__init__(groups)
         self.image = surface
 
         (x,y) = random.randint(0, SCREEN_WIDTH), 0
         self.rect: pg.FRect = self.image.get_frect(midbottom = (x,y))
 
-        self.speed = 200
+        self.speed = 400
         self.creation_time = pg.time.get_ticks()
-        self.lifetime = 2000
+        self.lifetime = 3000
+        self.direction = pg.Vector2(random.uniform(-0.5, 0.5), 1)
 
     def update(self, dt: int):
-        self.rect.centery += self.speed * dt
+        self.rect.center += self.direction * self.speed * dt
         current_time = pg.time.get_ticks()
         if current_time - self.creation_time >= self.lifetime:
             self.kill()
 
+
+def collisions():
+    global running
+
+    player_collisions = pg.sprite.spritecollide(player, meteor_sprites, True)
+    if player_collisions:
+        running = False
+
+    for laser_sprite in laser_sprites:
+        meteor_collisions = pg.sprite.spritecollide(laser_sprite,
+                                                    meteor_sprites, True)
+        if meteor_collisions:
+            laser_sprite.kill()
+
+
+def display_score():
+    game_time = pg.time.get_ticks() // 100
+
+    font = pg.font.Font(None, 50)
+    text_surface = font.render(str(game_time), True, (240,240,240))
+    text_rect: pg.FRect = text_surface.get_frect(
+        midbottom = (SCREEN_WIDTH / 2, SCREEN_HEIGHT - 50))
+    display_surface.blit(text_surface, text_rect)
+    pg.draw.rect(display_surface, (240,240,240),
+                 text_rect.inflate(30,15).move(0, -1), 4, 5)
+
+
+# window constants
+(SCREEN_WIDTH, SCREEN_HEIGHT) = 1280, 720
+SCREEN_CENTER: tuple[float,float] = SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2
 
 # setup
 pg.init()
@@ -101,7 +131,7 @@ pg.display.set_caption("Space Shooter")
 running = True
 clock: pg.Clock = pg.time.Clock()
 
-# load surfaces
+# import
 player_surface = pg.image.load(
     os.path.join('images', 'player.png')).convert_alpha()
 
@@ -117,7 +147,10 @@ meteor_surface = pg.image.load(
 all_sprites: pg.sprite.Group[pg.sprite.Sprite] = pg.sprite.Group()
 for i in range(0, 20):
     Star(star_surface, all_sprites)
-player = Player(player_surface, all_sprites)
+player = Player(player_surface, laser_surface, all_sprites)
+
+meteor_sprites: pg.sprite.Group[pg.sprite.Sprite] = pg.sprite.Group()
+laser_sprites: pg.sprite.Group[pg.sprite.Sprite] = pg.sprite.Group()
 
 # custom events -> meteor event
 meteor_event = pg.event.custom_type()
@@ -131,13 +164,16 @@ while running:
         if event.type == pg.QUIT:
             running = False
         if event.type == meteor_event:
-            Meteor(meteor_surface, all_sprites)
+            Meteor(meteor_surface, [all_sprites, meteor_sprites])
 
     all_sprites.update(dt)
 
     # fill screen with color to wipe last frame
-    display_surface.fill("darkgrey")
+    display_surface.fill("#3a2e3f")
     all_sprites.draw(display_surface)
+
+    display_score()
+    collisions()
 
     pg.display.update()
 
